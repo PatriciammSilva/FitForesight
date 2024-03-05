88ce2556e7a17b85b5ab9e2b246e6a6b322e5d2e
PatriciammSilva <pmmsilva1998@gmail.com>
2024-03-05 13:31:48 +0000

    new

--- src/dados1.py
+++ src/dados1.py
@@ -2,12 +2,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+#import scikit-learn 
+
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
 
 dados = pd.read_csv('/Users/patriciasilva/Desktop/Dados 1 - Experiência/dados1.csv')
- # cabeçalho da base de dados
+   
+   #cabeçalho da base de dados
 print(dados.head())
+
  # dimensões da base de dados
 print(dados.shape)
+
  # substituir valores NA pela mediana da variável
 print(dados.isna().sum())
 dados['age'] = dados['age'].fillna(dados['age'].median())
@@ -15,50 +22,63 @@ dados['height'] = dados['height'].fillna(dados['height'].median())
 print(dados.isna().sum())
 print(dados.head())
 print(dados.shape)
+
    # algumas estatísticas descritivas dos dados
 print(dados.describe())
+
    # descrição da variável categórica SIZE
    # 'object' tipo da variável SIZE
 print(dados.describe(include='object'))
+ 
    # gráficos boxplot
-sns.boxplot(dados['weight'])
-sns.boxplot(dados['age'])
-sns.boxplot(dados['height'])
+sns.boxplot([dados['weight'], dados['age'], dados['height']])
 print(plt.show())
-#plt.boxplot(dados['weight'])
-#plt.boxplot(dados['age'])
-#plt.boxplot(dados['height'])
-#print(plt.show())
+plt.boxplot([dados['weight'], dados['age'], dados['height']])  
+print(plt.show())   
+                                         
    # os três boxplots apresentam outliers
+ 
    # identificar valores outliers na variável weight
 Q1 = dados['weight'].quantile(0.25)
 Q3 = dados['weight'].quantile(0.75)
 IQR = Q3 - Q1
 outw = dados[(dados['weight'] < Q1 - 1.5 * IQR) | (dados['weight'] > Q3 + 1.5 * IQR)]
 print(outw)
+print(outw.shape)
+   # 3510 valores outliers
+
    # identificar valores outliers na variável age
 Q1 = dados['age'].quantile(0.25)
 Q3 = dados['age'].quantile(0.75)
 IQR = Q3 - Q1
-outw = dados[(dados['age'] < Q1 - 1.5 * IQR) | (dados['age'] > Q3 + 1.5 * IQR)]
+outa = dados[(dados['age'] < Q1 - 1.5 * IQR) | (dados['age'] > Q3 + 1.5 * IQR)]
 print(outa)
+print(outa.shape)
+   # 6726 valores outliers
+
    # identificar valores outliers na variável height
 Q1 = dados['height'].quantile(0.25)
 Q3 = dados['height'].quantile(0.75)
 IQR = Q3 - Q1
-outw = dados[(dados['height'] < Q1 - 1.5 * IQR) | (dados['height'] > Q3 + 1.5 * IQR)]
-print(outw)
-   # remover outliers das variáveis quantitativas
-dados1 = dados[~((dados['coluna'] < Q1 - 1.5 * IQR) | (dados['coluna'] > Q3 + 1.5 * IQR))]
+outh= dados[(dados['height'] < Q1 - 1.5 * IQR) | (dados['height'] > Q3 + 1.5 * IQR)]
+print(outh)
+print(outh.shape)
+   # 185 valores outliers
+ 
+   # gráficos da variável Size
+#sns.countplot(dados['size'])
+size1 = [9731, 21127, 28379, 16533, 17747, 63, 16025]
+bars = ('XXS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL')
+pos = np.arange(len(bars))
+plt.bar(pos, size1)
+print(plt.show())  
+sns.countplot(dados['size'])
+print(plt.show())  
+plt.pie(size1)
+print(plt.show()) 
 
-print(dados1)
-   # gráficos boxplot sem outliers 
-sns.boxplot(dados1['weight'])
-sns.boxplot(dados1['age'])
-sns.boxplot(dados1['height'])
-print(plt.show())
-   # dimensões da base de dados sem valores outliers 
-print(dados1.shape)
-   # gráfico de barras da variável SIZE
-sns.countplot(dados1['size'],palette='gist_stern')
-print(plt.show())
+   # kmeans com os valores outliers 
+data = pd.get_dummies(dados,columns=['size'])     
+data.head()
+features = data.drop(['size_L','size_M','size_S','size_XL','size_XXL','size_XXS','size_XXXL'], axis=1)
+features.head()
