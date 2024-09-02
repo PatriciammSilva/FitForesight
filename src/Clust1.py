50a8b2081a4b7cc884b8f861ab611cd7c57b00bc
PatriciammSilva <pmmsilva1998@gmail.com>
2024-09-02 16:56:43 +0100

    .

--- src/Clust1.py
+++ src/Clust1.py
@@ -1,4 +1,4 @@
-## Previsões Mod1 - 0.60
+## cluster - 0.60
    # alterar o número do dataset no comando de importação
 
 
@@ -15,22 +15,16 @@ import joblib
 kmeans = joblib.load('modkmeans.pkl')
 df1 = pd.read_csv('/Users/patriciasilva/Desktop/Tese/FitForesight/Datasets/df1.csv')
 
-## Normalizar dataset
-scaler = StandardScaler()
-df1nor = scaler.fit_transform(df1)
+
+## Cluster treinado
+labels = kmeans.predict(dfnor)
+print(labels)
 
 
 ## Previsão
 kmeans = joblib.load('modkmeans.pkl')
 labels = kmeans.predict(df1nor)
-print(labels)
 np.set_printoptions(threshold=np.inf)
 print(labels)
 cluster_counts = pd.Series(labels).value_counts()
 print(cluster_counts)
-
-## Gráfico
-plt.scatter(df1nor[:, 0], df1nor[:, 1], c=labels, s=50, cmap='viridis')
-cent = kmeans.cluster_centers_
-plt.scatter(cent[:, 0], cent[:, 1], c='red', s=200, alpha=0.75, marker='X')
-plt.show()
