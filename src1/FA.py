f5e149f965b683349baa88f443bc16cd9b7ef0ef
PatriciammSilva <pmmsilva1998@gmail.com>
2024-09-17 11:04:03 +0100

    .

---
+++ src1/FA.py
@@ -0,0 +1,20 @@
+## FA
+
+   
+## Packages necessários
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import FactorAnalysis
+
+
+## Importação Dataset
+df = pd.read_csv('/Users/patriciasilva/Desktop/Tese/FitForesight/Datasets1/data1.csv')
+
+## Normalizar dataset
+scaler = StandardScaler()
+dfnor = scaler.fit_transform(df)
+
+## Aplicar FA
+fa = FactorAnalysis(n_components=4, random_state=42)
+fact = fa.fit_transform(dfnor)
+print(fact)
